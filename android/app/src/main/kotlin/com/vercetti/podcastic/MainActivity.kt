98474fdb7bb652e7f084e2498d7c8bbe65aa7742
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-04 11:07:58 +0300

    Project initialization

---
+++ android/app/src/main/kotlin/com/vercetti/podcastic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vercetti.podcastic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PodcastApp());
+}
+
+class PodcastApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
