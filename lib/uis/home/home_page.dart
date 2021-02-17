f10cf6658b3a6a9cc8d6fafd81c11cd8c156b1db
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-17 12:33:40 +0300

    Added main home layout

--- lib/uis/home/home_page.dart
+++ lib/uis/home/home_page.dart
@@ -1,8 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoTabScaffold(
+      tabBuilder: (context, index) => Container(
+          // color: Colors.red,
+          ),
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.lightbulb), label: 'Explore'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.list_dash), label: 'My podcasts'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings), label: 'Settings'),
+        ],
+      ),
+    );
   }
 }
