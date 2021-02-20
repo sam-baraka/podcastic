dc8a6ee6ca3b50d9b0ac7369182354fde7895ecf
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-21 00:03:24 +0300

    Changed primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:podcastic/colors/app_colors.dart';
 import 'package:podcastic/routes/routes.gr.dart';
 import 'package:podcastic/routes/routes.gr.dart' as r;
 
@@ -12,6 +13,7 @@ class PodcastApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
         title: 'Podcastic',
+        theme: CupertinoThemeData(primaryColor: AppColors.appLightGreenColor),
         builder: ExtendedNavigator.builder<r.Router>(
             router: r.Router(), initialRoute: Routes.homePage));
   }
