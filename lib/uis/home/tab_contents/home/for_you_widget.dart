a582d6abeae5b7aeee99547a655a36209bf64418
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-20 15:45:07 +0300

    Designed the for you widget

--- lib/uis/home/tab_contents/home/for_you_widget.dart
+++ lib/uis/home/tab_contents/home/for_you_widget.dart
@@ -3,6 +3,30 @@ part of 'home.dart';
 class ForYouWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ConstrainedBox(
+      constraints: BoxConstraints(minHeight: 200, maxHeight: 200),
+      child: ListView.separated(
+        itemCount: 5,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => ForYouCard(),
+        separatorBuilder: (context, index) => SizedBox(
+          width: 8,
+        ),
+      ),
+    );
+  }
+}
+
+class ForYouCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: Container(
+        height: 200,
+        width: 200,
+        color: Colors.pink,
+      ),
+    );
   }
 }
