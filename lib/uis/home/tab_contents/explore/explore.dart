f3527b4895aa7ece4230d23f07956a0ff9226c7b
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-20 23:41:57 +0300

    UI Display fix

--- lib/uis/home/tab_contents/explore/explore.dart
+++ lib/uis/home/tab_contents/explore/explore.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Explore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      child: Center(child: Text("Explore")),
+      child: Center(
+          child: Text(
+        "Explore",
+        style: GoogleFonts.redHatDisplay(
+          color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+              ? Colors.white
+              : Colors.black,
+        ),
+      )),
     );
     ;
   }
--- lib/uis/home/tab_contents/my_podcasts/my_podcasts.dart
+++ lib/uis/home/tab_contents/my_podcasts/my_podcasts.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyPodcasts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      child: Center(child: Text("My Podcasts")),
+      child: Center(
+          child: Text(
+        "My Podcasts",
+        style: GoogleFonts.redHatDisplay(
+          color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+              ? Colors.white
+              : Colors.black,
+        ),
+      )),
     );
   }
 }
