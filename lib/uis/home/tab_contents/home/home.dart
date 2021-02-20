563477488fc5f2fd7dbe58126cee17525ec1643a
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-20 15:44:53 +0300

    Integrated for you widget to home page

--- lib/uis/home/tab_contents/home/home.dart
+++ lib/uis/home/tab_contents/home/home.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+part 'for_you_widget.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      child: Center(child: Text("Home")),
-    );
+        child: CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle: Text("Podcastic", style: GoogleFonts.redHatDisplay()),
+        ),
+        SliverFillRemaining(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "For you",
+                style: GoogleFonts.redHatDisplay(
+                    color: CupertinoColors.activeBlue,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+              ForYouWidget(),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
