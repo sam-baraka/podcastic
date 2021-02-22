7c0e004abda3e66679561c82298ce0bce41a79a2
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-22 10:15:21 +0300

    Fixed color disparities

--- lib/uis/common/podcast_tile.dart
+++ lib/uis/common/podcast_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:podcastic/test_images/test_images.dart';
 
 class PodcastTile extends StatelessWidget {
@@ -8,9 +9,26 @@ class PodcastTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+          ? Colors.black
+          : Colors.white,
       child: ListTile(
-        title: Text("Tesla buys 1500000 in bit coin"),
-        subtitle: Text("Lew Later"),
+        title: Text(
+          "Tesla buys 1500000 in bit coin",
+          style: GoogleFonts.redHatDisplay(
+            color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+                ? Colors.white
+                : Colors.black,
+          ),
+        ),
+        subtitle: Text(
+          "Lew Later",
+          style: GoogleFonts.redHatDisplay(
+            color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+                ? Colors.white
+                : Colors.black,
+          ),
+        ),
       ),
     );
   }
