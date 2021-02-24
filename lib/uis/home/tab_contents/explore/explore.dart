e7b6a2aeadf0ae4ae5990ead9420000d6f35aea6
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-24 11:36:34 +0300

    Added search

--- lib/uis/home/tab_contents/explore/explore.dart
+++ lib/uis/home/tab_contents/explore/explore.dart
@@ -3,19 +3,50 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Explore extends StatelessWidget {
+  var delegate = SliverPersistentHeaderDelegate;
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-          child: Text(
-        "Explore",
-        style: GoogleFonts.redHatDisplay(
-          color: CupertinoTheme.brightnessOf(context) == Brightness.dark
-              ? Colors.white
-              : Colors.black,
-        ),
-      )),
+        child: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) {
+        return [
+          CupertinoSliverNavigationBar(
+            largeTitle: Text("Explore", style: GoogleFonts.redHatDisplay()),
+          ),
+          SliverPersistentHeader(
+            delegate: _SliverAppBarDelegate(CupertinoTextField(
+              placeholder: "Search podcasts",
+            )),
+          )
+        ];
+      },
+      body: Column(
+        children: [],
+      ),
+    ));
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._child);
+
+  final Widget _child;
+
+  @override
+  double get minExtent => 50;
+  @override
+  double get maxExtent => 50;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _child,
     );
-    ;
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
