2bdd2c7617eccb4f4b3dfa2cf89d568d9ccf6861
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-03-07 18:57:21 +0300

    Added explore podcast cards

--- lib/uis/home/tab_contents/explore/explore.dart
+++ lib/uis/home/tab_contents/explore/explore.dart
@@ -3,50 +3,82 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Explore extends StatelessWidget {
-  var delegate = SliverPersistentHeaderDelegate;
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        child: NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) {
-        return [
-          CupertinoSliverNavigationBar(
-            largeTitle: Text("Explore", style: GoogleFonts.redHatDisplay()),
+        child: CustomScrollView(
+      slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle:
+              Text("Explore podcasts", style: GoogleFonts.redHatDisplay()),
+        ),
+        SliverFillRemaining(
+          child: Column(
+            children: [
+              // Container(
+              //   decoration:
+              //       BoxDecoration(border: Border.all(color: Colors.grey)),
+              //   child: CupertinoTextFormFieldRow(
+              //     prefix: Icon(CupertinoIcons.mic),
+              //     placeholder: 'Podcast name, topic, category...',
+              //     placeholderStyle: TextStyle(
+              //         color: CupertinoTheme.brightnessOf(context) ==
+              //                 Brightness.dark
+              //             ? Colors.white
+              //             : Colors.grey),
+              //   ),
+              // ),
+              Flexible(
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: (MediaQuery.of(context).size.height /
+                              MediaQuery.of(context).size.width)
+                          .ceil()),
+                  children: [
+                    Category(
+                      gradient:
+                          LinearGradient(colors: [Colors.red, Colors.blue]),
+                    ),
+                    Category(
+                      gradient:
+                          LinearGradient(colors: [Colors.yellow, Colors.green]),
+                    ),
+                    Category(
+                      gradient:
+                          LinearGradient(colors: [Colors.cyan, Colors.blue]),
+                    ),
+                    Category(
+                      gradient: LinearGradient(
+                          colors: [Colors.pink, Colors.greenAccent]),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          SliverPersistentHeader(
-            delegate: _SliverAppBarDelegate(CupertinoTextField(
-              placeholder: "Search podcasts",
-            )),
-          )
-        ];
-      },
-      body: Column(
-        children: [],
-      ),
+        )
+      ],
     ));
   }
 }
 
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._child);
+class Category extends StatelessWidget {
+  final LinearGradient gradient;
 
-  final Widget _child;
-
+  const Category({Key key, this.gradient}) : super(key: key);
   @override
-  double get minExtent => 50;
-  @override
-  double get maxExtent => 50;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      child: _child,
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5), gradient: gradient),
+        height: 50,
+        child: ListTile(
+          title: Text(
+            "Comedy",
+          ),
+        ),
+      ),
     );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
   }
 }
