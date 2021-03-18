03f0e4178201e6560abd473d909106809afdd2a5
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-03-18 22:39:26 +0300

    Added custom sliver app bar delegate

---
+++ lib/uis/common/sliver_app_bar_delegate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
