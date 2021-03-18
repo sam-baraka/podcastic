9959bda658570207c9f9a37f605b8ddb4c7850e9
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-03-18 23:21:46 +0300

    Added dummy data

--- lib/uis/home/tab_contents/my_podcasts/my_podcasts.dart
+++ lib/uis/home/tab_contents/my_podcasts/my_podcasts.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:podcastic/uis/uis.dart';
 
 class MyPodcasts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-          child: Text(
-        "My Podcasts",
-        style: GoogleFonts.redHatDisplay(
-          color: CupertinoTheme.brightnessOf(context) == Brightness.dark
-              ? Colors.white
-              : Colors.black,
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("My podcasts"),
         ),
-      )),
-    );
+        child: ListView(
+          children: [
+            PodcastTile(),
+            PodcastTile(),
+            PodcastTile(),
+          ],
+        ));
   }
 }
