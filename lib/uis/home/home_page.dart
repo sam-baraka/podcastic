b32b56a809de8a393f042155fefa21a373b9609d
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-03-18 23:33:42 +0300

    Added now playing ui

--- lib/uis/home/home_page.dart
+++ lib/uis/home/home_page.dart
@@ -42,83 +42,189 @@ class HomePage extends StatelessWidget {
           left: 0,
           right: 0,
           bottom: 50,
-          child: Material(
-            color: CupertinoTheme.brightnessOf(context) == Brightness.dark
-                ? Colors.black87
-                : Colors.white,
-            child: ListTile(
-              contentPadding: EdgeInsets.all(4),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Container(
-                    height: 50,
-                    width: 50,
-                    child: Image.asset(
-                      TestImages.getRandomImage(),
-                      fit: BoxFit.fitHeight,
-                    )),
-              ),
-              trailing: CupertinoButton(
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  //TODO: Add on pressed
-                },
-                child: Icon(
-                  CupertinoIcons.pause,
-                  size: 40,
+          child: GestureDetector(
+            onTap: () {
+              showCupertinoModalPopup(
+                  context: context,
+                  builder: (builder) => CupertinoPopupSurface(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 200,
+                              width: 200,
+                              child: Image.asset(
+                                TestImages.getRandomImage(),
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                            Container(
+                              height: 20,
+                              child: Marquee(
+                                text:
+                                    "All about the perseverance rover by Samuel Baraka",
+                                scrollAxis: Axis.horizontal,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                blankSpace: 20.0,
+                                velocity: 100.0,
+                                pauseAfterRound: Duration(seconds: 1),
+                                startPadding: 10.0,
+                                accelerationDuration: Duration(seconds: 1),
+                                accelerationCurve: Curves.linear,
+                                decelerationDuration:
+                                    Duration(milliseconds: 500),
+                                decelerationCurve: Curves.easeOut,
+                                style: GoogleFonts.redHatDisplay(
+                                    color:
+                                        CupertinoTheme.brightnessOf(context) ==
+                                                Brightness.dark
+                                            ? Colors.white
+                                            : Colors.black,
+                                    fontSize: 16),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: CupertinoButton(
+                                    padding: EdgeInsets.all(0),
+                                    onPressed: () {
+                                      //TODO: Add on pressed
+                                    },
+                                    child: Icon(
+                                      CupertinoIcons.backward_end_alt,
+                                      size: 40,
+                                    ),
+                                  ),
+                                ),
+                                CupertinoButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () {
+                                    //TODO: Add on pressed
+                                  },
+                                  child: Icon(
+                                    CupertinoIcons.gobackward_10,
+                                    size: 40,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: CupertinoButton(
+                                    padding: EdgeInsets.all(0),
+                                    onPressed: () {
+                                      //TODO: Add on pressed
+                                    },
+                                    child: Icon(
+                                      CupertinoIcons.play,
+                                      size: 40,
+                                    ),
+                                  ),
+                                ),
+                                CupertinoButton(
+                                  padding: EdgeInsets.all(0),
+                                  onPressed: () {
+                                    //TODO: Add on pressed
+                                  },
+                                  child: Icon(
+                                    CupertinoIcons.goforward_10,
+                                    size: 40,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: CupertinoButton(
+                                    padding: EdgeInsets.all(0),
+                                    onPressed: () {
+                                      //TODO: Add on pressed
+                                    },
+                                    child: Icon(
+                                      CupertinoIcons.forward_end_alt,
+                                      size: 40,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ));
+            },
+            child: Material(
+              color: CupertinoTheme.brightnessOf(context) == Brightness.dark
+                  ? Colors.black87
+                  : Colors.white,
+              child: ListTile(
+                contentPadding: EdgeInsets.all(4),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                      height: 50,
+                      width: 50,
+                      child: Image.asset(
+                        TestImages.getRandomImage(),
+                        fit: BoxFit.fitHeight,
+                      )),
                 ),
-              ),
-              title: Container(
-                height: 20,
-                child: Marquee(
-                  text: "All about the perseverance rover by Samuel Baraka",
-                  scrollAxis: Axis.horizontal,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  blankSpace: 20.0,
-                  velocity: 100.0,
-                  pauseAfterRound: Duration(seconds: 1),
-                  startPadding: 10.0,
-                  accelerationDuration: Duration(seconds: 1),
-                  accelerationCurve: Curves.linear,
-                  decelerationDuration: Duration(milliseconds: 500),
-                  decelerationCurve: Curves.easeOut,
-                  style: GoogleFonts.redHatDisplay(
-                      color: CupertinoTheme.brightnessOf(context) ==
-                              Brightness.dark
-                          ? Colors.white
-                          : Colors.black,
-                      fontSize: 16),
+                trailing: CupertinoButton(
+                  padding: EdgeInsets.all(0),
+                  onPressed: () {
+                    //TODO: Add on pressed
+                  },
+                  child: Icon(
+                    CupertinoIcons.pause,
+                    size: 40,
+                  ),
                 ),
+                title: Container(
+                  height: 20,
+                  child: Marquee(
+                    text: "All about the perseverance rover by Samuel Baraka",
+                    scrollAxis: Axis.horizontal,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    blankSpace: 20.0,
+                    velocity: 100.0,
+                    pauseAfterRound: Duration(seconds: 1),
+                    startPadding: 10.0,
+                    accelerationDuration: Duration(seconds: 1),
+                    accelerationCurve: Curves.linear,
+                    decelerationDuration: Duration(milliseconds: 500),
+                    decelerationCurve: Curves.easeOut,
+                    style: GoogleFonts.redHatDisplay(
+                        color: CupertinoTheme.brightnessOf(context) ==
+                                Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
+                        fontSize: 16),
+                  ),
+                ),
+                // subtitle: Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     CupertinoButton(
+                //         padding: EdgeInsets.all(4),
+                //         onPressed: () {},
+                //         child: Icon(
+                //           Icons.replay_10_outlined,
+                //           size: 30,
+                //         )),
+                //     SizedBox(
+                //       width: 20,
+                //     ),
+                //     CupertinoButton(
+                //         padding: EdgeInsets.all(4),
+                //         onPressed: () {},
+                //         child: Icon(
+                //           CupertinoIcons.play,
+                //           size: 40,
+                //         )),
+                //     SizedBox(
+                //       width: 20,
+                //     ),
+                //     CupertinoButton(
+                //         padding: EdgeInsets.all(4),
+                //         onPressed: () {},
+                //         child: Icon(Icons.forward_10_outlined, size: 30)),
+                //   ],
+                // )
               ),
-              // subtitle: Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     CupertinoButton(
-              //         padding: EdgeInsets.all(4),
-              //         onPressed: () {},
-              //         child: Icon(
-              //           Icons.replay_10_outlined,
-              //           size: 30,
-              //         )),
-              //     SizedBox(
-              //       width: 20,
-              //     ),
-              //     CupertinoButton(
-              //         padding: EdgeInsets.all(4),
-              //         onPressed: () {},
-              //         child: Icon(
-              //           CupertinoIcons.play,
-              //           size: 40,
-              //         )),
-              //     SizedBox(
-              //       width: 20,
-              //     ),
-              //     CupertinoButton(
-              //         padding: EdgeInsets.all(4),
-              //         onPressed: () {},
-              //         child: Icon(Icons.forward_10_outlined, size: 30)),
-              //   ],
-              // )
             ),
           ),
         )
