cd8425894cad46dc8aa93ec440a2ca851b252ead
Samuel Baraka <sababuvercetti@gmail.com>
2021-11-16 13:14:13 +0300

    Upgrded dependencies and app reconfiguration

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:podcastic/colors/app_colors.dart';
+import 'package:podcastic/config/config.dart';
 import 'package:podcastic/routes/routes.gr.dart';
 
 void main() {
@@ -23,7 +23,7 @@ class PodcastApp extends StatelessWidget {
       routerDelegate: _appRouter.delegate(),
       title: 'Podcastic',
       theme: CupertinoThemeData(
-          primaryColor: AppColors.appLightGreenColor,
+          primaryColor: Palette.appLightGreenColor,
           textTheme:
               CupertinoTextThemeData(textStyle: GoogleFonts.redHatDisplay())),
     );
--- lib/routes/routes.gr.dart
+++ lib/routes/routes.gr.dart
@@ -1,29 +1,37 @@
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i1;
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:flutter/material.dart' as _i3;
 
-import '../uis/home/home_page.dart' as _i2;
+import '../uis/home/home_page.dart' as _i1;
 
-class AppRouter extends _i1.RootStackRouter {
-  AppRouter();
+class AppRouter extends _i2.RootStackRouter {
+  AppRouter([_i3.GlobalKey<_i3.NavigatorState>? navigatorKey])
+      : super(navigatorKey);
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {
-    HomeRoute.name: (entry) {
-      return _i1.CupertinoPageX(entry: entry, child: _i2.HomePage());
+  final Map<String, _i2.PageFactory> pagesMap = {
+    HomeRoute.name: (routeData) {
+      return _i2.CupertinoPageX<dynamic>(
+          routeData: routeData, child: _i1.HomePage());
     }
   };
 
   @override
-  List<_i1.RouteConfig> get routes =>
-      [_i1.RouteConfig(HomeRoute.name, path: '/')];
+  List<_i2.RouteConfig> get routes =>
+      [_i2.RouteConfig(HomeRoute.name, path: '/')];
 }
 
-class HomeRoute extends _i1.PageRouteInfo {
+/// generated route for [_i1.HomePage]
+class HomeRoute extends _i2.PageRouteInfo<void> {
   const HomeRoute() : super(name, path: '/');
 
   static const String name = 'HomeRoute';
